f95146adda4b7646ddf1271040a000f07223a0a7
xuwenzhi <358350782@qq.com>
2015-09-06 10:12:25 +0800

    first commit

---
+++ Factory.class.php
@@ -0,0 +1,73 @@
+﻿<?php
+/**
+ * 工厂设计模式： 提供获取某个对象的新实例的一个接口， 同时使调用代码避免确定实际实例化基类的步骤
+ */
+//基础标准CD类
+class CD {
+	
+	public $tracks = array();
+	public $band   = '';
+	public $title  = '';
+	
+	public function __construct() {}
+	
+	public function setTitle($title) {
+		$this->title    = $title;
+	}
+	
+	public function setBand($band) {
+		$this->band     = $band;
+	}
+	
+	public function addTrack($track) {
+		$this->tracks[] = $track;
+	}
+	
+}
+
+//增强型CD类， 与标准CD的唯一不同是写至CD的第一个track是数据track("DATA TRACK")
+class enhadcedCD {
+
+	public $tracks = array();
+	public $band   = '';
+	public $title  = '';
+
+	public function __construct() {
+		$this->tracks   = "DATA TRACK";
+	}
+	
+	public function setTitle($title) {
+		$this->title    = $title;
+	}
+	
+	public function setBand($band) {
+		$this->band     = $band;
+	}
+	
+	public function addTrack($track) {
+		$this->tracks[] = $track;
+	}
+}
+
+//CD工厂类，实现对以上两个类具体实例化操作
+class CDFactory {
+	
+	public static function create($type) {
+		$class = strtolower($type) . "CD";
+		
+		return new $class;
+	}
+}
+
+//实例操作
+$type = "enhadced";
+
+$cd   = CDFactory::create($type);
+
+$tracksFromExternalSource = array("What It Means", "Brr", "Goodbye");
+
+$cd->setBand("Never Again");
+$cd->setTitle("Waste of a Rib");
+foreach ($tracksFromExternalSource as $track) {
+	$cd->addTrack($track);
+}
---
+++ SinglnInstance.class.php
@@ -0,0 +1,70 @@
+﻿<?php
+/**
+ * 单例模式： 通过提供对自身共享实例的访问， 单元素设计模式用于限制特定对象只能被创建一次
+ * 单例设计模式最常用于构建数据库连接对象。 
+ *
+ * 代码示例 - 背景介绍
+ * 利用单元素设计模式连接数据库并完成CD库存表记录更新
+ */
+//利用单元素设计模式编写的库存记录更新表
+class InventoryConnection {
+	
+	protected static $_instance = NULL;		//注意这个 static 的定义
+	
+	protected $_handle = NULL;
+	
+	public static function getInstance() {	//注意这个 static 的定义
+		if ( ! self::$_instance instanceof self) {
+			self::$_instance = new self;
+		}
+		
+		return self::$_instance;
+	}
+	
+	protected function __construct() {
+		$this->_handle = mysql_connect("localhost", "root", "root");
+		mysql_select_db("test", $this->_handle);
+	}
+	
+	public function updateQuantity($band, $title, $number) {
+		$query  = " update cd set amount = (amount + ". intval($number) . ")";
+		$query .= " where band = '";
+		$query .= mysql_real_escape_string($band);
+		$query .= "'";
+		$query .= " and title = '";
+		$query .= mysql_real_escape_string($title);
+		$query .= "'";
+		
+		mysql_query($query, $this->_handle);
+	}
+}
+
+//基本CD类
+class CD {
+	
+	protected $_title = "";
+	protected $_band  = "";
+	
+	public function __construct($title, $band) {
+		$this->_title = $title;
+		$this->_band  = $band;
+	}
+	
+	public function buy() {
+		$inventory = InventoryConnection::getInstance();
+		$inventory->updateQuantity($this->_band, $this->_title, -1);
+	}	
+}
+
+//测试实例
+$boughtCDs   = array();
+$boughtCDs[] = array("band" => "Never Again", "title" => "Waster of a Rib");
+$boughtCDs[] = array("band" => "Therapee", "title" => "Long Road");
+
+//因为会多次利用$cd执行buy操作, 所以在其buy方法具体实现的InventoryConnection对象
+//最好是单元素对象。
+//针对每个被购买的CD都打开一个与数据库的新连接并不是一个好做法。
+foreach ($boughtCDs as $boughtCD) {
+	$cd = new CD($boughtCD['title'], $boughtCD['band']);
+	$cd->buy();
+}
