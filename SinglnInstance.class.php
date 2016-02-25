403396ec31b91c28f83f9d40271843d39789f5b3
xuwenzhi <azxuwen701@gmail.com>
2016-02-25 22:18:07 +0800

    UP

--- SinglnInstance.class.php
+++ SinglnInstance.class.php
@@ -7,61 +7,64 @@
  * 利用单元素设计模式连接数据库并完成CD库存表记录更新
  */
 //利用单元素设计模式编写的库存记录更新表
-class InventoryConnection {
-	
-	protected static $_instance = NULL;		//注意这个 static 的定义
-	
-	protected $_handle = NULL;
-	
-	public static function getInstance() {	//注意这个 static 的定义
-		if ( ! self::$_instance instanceof self) {
-			self::$_instance = new self;
-		}
-		
-		return self::$_instance;
-	}
-	
-	protected function __construct() {
-		$this->_handle = $this->_retResType();
-		//mysql_select_db("test", $this->_handle);
-	}
+class Singleton
+{
+    /**
+     * @var Singleton The reference to *Singleton* instance of this class
+     */
+    private static $instance;
+    
+    /**
+     * Returns the *Singleton* instance of this class.
+     *
+     * @return Singleton The *Singleton* instance.
+     */
+    public static function getInstance()
+    {
+        if (null === self::$instance) {
+            static::$instance = new static();
+        }
+        
+        return self::$instance;
+    }
 
-	private function _retResType(){
-		$res = true;
-		return $res;
-	}
+    /**
+     * Protected constructor to prevent creating a new instance of the
+     * *Singleton* via the `new` operator from outside of this class.
+     */
+    protected function __construct()
+    {
+    }
 
-	public function update($band, $title){
-		echo "update!";
-	}
+    /**
+     * Private clone method to prevent cloning of the instance of the
+     * *Singleton* instance.
+     *
+     * @return void
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * Private unserialize method to prevent unserializing of the *Singleton*
+     * instance.
+     *
+     * @return void
+     */
+    private function __wakeup()
+    {
+    }
 }
 
-//基本CD类
-class CD {
-	
-	protected $_title = "";
-	protected $_band  = "";
-	
-	public function __construct($title, $band) {
-		$this->_title = $title;
-		$this->_band  = $band;
-	}
-	
-	public function buy() {
-		$inventory = InventoryConnection::getInstance();
-		$inventory->update($this->_band, $this->_title, -1);
-	}	
+class SingletonChild extends Singleton
+{
 }
 
-//测试实例
-$boughtCDs   = array();
-$boughtCDs[] = array("band" => "Never Again", "title" => "Waster of a Rib");
-$boughtCDs[] = array("band" => "Therapee", "title" => "Long Road");
+$obj = Singleton::getInstance();
+var_dump($obj === Singleton::getInstance());             // bool(true)
 
-//因为会多次利用$cd执行buy操作, 所以在其buy方法具体实现的InventoryConnection对象
-//最好是单元素对象。
-//针对每个被购买的CD都打开一个与数据库的新连接并不是一个好做法。
-foreach ($boughtCDs as $boughtCD) {
-	$cd = new CD($boughtCD['title'], $boughtCD['band']);
-	$cd->buy();
-}
+$anotherObj = SingletonChild::getInstance();
+var_dump($anotherObj === Singleton::getInstance());      // bool(false)
+
+var_dump($anotherObj === SingletonChild::getInstance()); // bool(true)
